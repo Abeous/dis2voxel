6519d9f2d5145481a4ffd947b016866e3c2634fc
abeous <abeouss@gmail.com>
2021-03-25 00:40:58 -0400

    init commmit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace dis2voxel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Image image = Image.FromFile(args[0]);
+            Map map = new(image);
+
+            string matrixString = "";
+
+            int matrixXLength = map.bitmapMatrix.GetLength(0);
+            int matrixYLength = map.bitmapMatrix.GetLength(1);
+
+            for (int y = 0; y < matrixXLength; y++)
+            {
+                for (int x = 0; x < matrixXLength; x++)
+                {
+                    if (x != 0)
+                    {
+                        matrixString += " " + map.bitmapMatrix[x, y];
+                    } else
+                    {
+                        matrixString += "\n" + map.bitmapMatrix[x, y];
+                    }
+                }
+
+            }
+
+            Console.WriteLine(matrixString);
+        }
+    }
+
+
+}
---
+++ src/Map.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace dis2voxel
+{
+    class Map
+    {
+        private Bitmap bitmap;
+        public int[,] bitmapMatrix;
+        public Map(Image image)
+        {
+            bitmap = new Bitmap(image);
+            bitmapMatrix = Map.MatrixFromBitmap(bitmap, "red");
+        }
+
+        static int[,] MatrixFromBitmap(Bitmap bitmap, string color)
+        {
+            int [,] bitmapMatrix = new int[bitmap.Width, bitmap.Height];
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    switch (color)
+                    {
+                        case "red":
+                            bitmapMatrix[x, y] = bitmap.GetPixel(x, y).R;
+                            break;
+                        case "blue":
+                            bitmapMatrix[x, y] = bitmap.GetPixel(x, y).B;
+                            break;
+                        case "green":
+                            bitmapMatrix[x, y] = bitmap.GetPixel(x, y).G;
+                            break;
+                        case "alpha":
+                            bitmapMatrix[x, y] = bitmap.GetPixel(x, y).A;
+                            break;
+                    }
+                }
+            }
+
+            return bitmapMatrix;
+        }
+    }
+}
