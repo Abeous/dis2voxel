bdb9ceeea0a1e859778c25c1c6b6038ba8e8e30f
abeous <abeouss@gmail.com>
2021-03-26 04:40:28 -0400

    add averaging sectioned arrays functionality

--- Program.cs
+++ Program.cs
@@ -13,8 +13,27 @@ namespace dis2voxel
 
             int[,] cutArray = Map.TrimFat(map.Bitmap2DArray, averagedSections);
             Console.WriteLine(Map.Int2DArrayToString(cutArray));
-            Console.WriteLine(cutArray.GetLength(0));
 
+            int[,] newArray = new int[averagedSections,averagedSections];
+            if (averagedSections > (cutArray.GetLength(0) / 2))
+            {
+                newArray = cutArray;
+            } else
+            {
+                int sectionLength = (cutArray.GetLength(0) / averagedSections);
+                for (int posY = 0; posY < averagedSections; posY++)
+                {
+                    for (int posX = 0; posX < averagedSections; posX++)
+                    {
+                        int startX = posX * sectionLength;
+                        int startY = posY * sectionLength;
+                        int[,] newSection = Map.GetSection(cutArray, startX, startY, sectionLength);
+                        newArray[posX, posY] = Map.Average2DArrayValues(newSection);
+                    }
+
+                }
+            }
+            Console.WriteLine(Map.Int2DArrayToString(newArray));
         }
     }
 
--- src/Map.cs
+++ src/Map.cs
@@ -67,7 +67,7 @@ namespace dis2voxel
             int matrixXLength = array.GetLength(0);
             int matrixYLength = array.GetLength(1);
 
-            for (int y = 0; y < matrixXLength; y++)
+            for (int y = 0; y < matrixYLength; y++)
             {
                 for (int x = 0; x < matrixXLength; x++)
                 {
@@ -87,6 +87,9 @@ namespace dis2voxel
 
         public static int[,] TrimFat(int[,] array, int sections)
         {
+            // check if trimming needs to be done
+            if (array.GetLength(0) % sections == 0) { return array; };
+
             int[] arrayLength = { array.GetLength(0), array.GetLength(1)};
             int[] arrayFatLength = { arrayLength[0] % sections, arrayLength[1] % sections };
             int[] arrayCutLength = { arrayLength[0] - arrayFatLength[0], arrayLength[1] - arrayFatLength[1] };
@@ -98,11 +101,24 @@ namespace dis2voxel
                 for (int x = 0; x < arrayLength[0] - arrayFatLength[0]; x++)
                 {
                     cutArray[x, y] = array[x, y];
-                    //cutArray[x, y] = 2;
                 }
             }
             return cutArray;
         }
 
+        public static int[,] GetSection(int[,] array, int startX, int startY, int sectionLength)
+        {
+            int[,] newArray = new int[sectionLength,sectionLength];
+
+            for (int y = startY; y < (startY + sectionLength); y++)
+            {
+                for (int x = startX; x < (startX + sectionLength); x++)
+                {
+                    newArray[x - startX, y - startY] = array[x , y];
+                }
+
+            }
+            return newArray;
+        }
     }
 }
